26af133cb8e351b21f5aa78fa9ae01cfcbd8c5da
GitdohunKim <134466864+GitdohunKim@users.noreply.github.com>
2023-12-22 12:57:53 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,13 @@
+def solution(number, k):
+    stack = []
+
+    for num in number:
+        while stack and k > 0 and stack[-1] < num:
+            stack.pop()
+            k -= 1
+
+        stack.append(num)
+        
+    stack = stack[:-k] if k > 0 else stack
+
+    return ''.join(stack)
