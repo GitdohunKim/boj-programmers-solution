5d8225e49d3163e4742d4a5742a922c395c73969
GitdohunKim <134466864+GitdohunKim@users.noreply.github.com>
2023-12-23 09:50:10 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,12 @@
+def solution(s):
+    words = s.split(" ") 
+    result = [] 
+    for word in words:
+        new_word = '' 
+        for idx, alp in enumerate(word):
+            if idx % 2 == 0:
+                new_word += alp.upper()
+            else:
+                new_word += alp.lower()
+        result.append(new_word)
+    return " ".join(result)
