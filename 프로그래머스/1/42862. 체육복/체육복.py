52fba78358a425fd8842d961f32f49f6dfca9fcd
GitdohunKim <134466864+GitdohunKim@users.noreply.github.com>
2024-01-02 15:29:34 +0900

    [level 1] Title: 체육복, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,12 @@
+def solution(n, lost, reserve):
+    real_lost = set(lost) - set(reserve)
+    real_reserve = set(reserve) - set(lost)
+
+    for student in real_reserve:
+        if student - 1 in real_lost:
+            real_lost.remove(student - 1)
+        elif student + 1 in real_lost:
+            real_lost.remove(student + 1)
+
+    answer = n - len(real_lost)
+    return answer
