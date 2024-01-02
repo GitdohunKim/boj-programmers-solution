1730b0320c4d80aa7a766c17ab548b88fa75cb15
GitdohunKim <134466864+GitdohunKim@users.noreply.github.com>
2024-01-03 02:48:27 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 396 ms, Memory: 64276 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -6,7 +6,6 @@ INF = int(1e9)
 v, e = map(int, input().split())
 graph = [[] for _ in range(v + 1)]
 
-# 방향성 없는 그래프이므로 x, y일 때와 y, x일 때 모두 추가
 for _ in range(e):
     x, y, cost = map(int, input().split())
     graph[x].append((y, cost))
@@ -32,13 +31,11 @@ def dijkstra(start):
                 distance[i[0]] = cost
                 heapq.heappush(q, (cost, i[0]))
 
-    # 반환값은 최단 거리 배열
     return distance
 
 
 v1, v2 = map(int, input().split())
 
-# 출발점이 각각 1, v1, v2일 때의 최단 거리 배열
 original_distance = dijkstra(1)
 v1_distance = dijkstra(v1)
 v2_distance = dijkstra(v2)
