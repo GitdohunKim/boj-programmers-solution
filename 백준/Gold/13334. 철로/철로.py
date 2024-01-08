8f64af077123c45a237d7f698d314782aef987b8
GitdohunKim <134466864+GitdohunKim@users.noreply.github.com>
2024-01-08 18:27:45 +0900

    [Gold II] Title: 철로, Time: 324 ms, Memory: 55044 KB -BaekjoonHub

--- 백준/Gold/13334. 철로/철로.py
+++ 백준/Gold/13334. 철로/철로.py
@@ -1,34 +1,26 @@
 from sys import stdin
 import heapq
 
-n = int(stdin.readline())
-roads, data = [], []
+def main():
+    n = int(stdin.readline())
+    data = [sorted(map(int, stdin.readline().split())) for _ in range(n)]
+    train_road_length = int(stdin.readline())
 
-for _ in range(n):
-    data.append(sorted(list(map(int, stdin.readline().split()))))
-train_road_length = int(stdin.readline())
+    roads = [road for road in data if road[1] - road[0] <= train_road_length]
+    roads.sort(key=lambda x: x[1])
 
+    answer = 0
+    q = []
 
-for road in data:
-    s, e = road
-    if (e - s) <= train_road_length:
-        roads.append(road)
-
-roads.sort(key=lambda x: x[1])
-
-answer = 0
-q = []
-
-for road in roads:
-    if not q:
-        heapq.heappush(q, road)
-    else:
-        while q[0][0] < road[1] - train_road_length:
+    for road in roads:
+        s, e = road
+        while q and q[0][0] < e - train_road_length:
             heapq.heappop(q)
-            if not q:
-                break
 
         heapq.heappush(q, road)
-    answer = max(answer, len(q))
+        answer = max(answer, len(q))
 
-print(answer)
+    print(answer)
+
+if __name__ == "__main__":
+    main()
