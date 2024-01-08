0959344030395bab610733e7da49091d32426645
GitdohunKim <134466864+GitdohunKim@users.noreply.github.com>
2024-01-08 17:59:54 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 6108 ms, Memory: 315852 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
@@ -4,27 +4,26 @@ sys.setrecursionlimit(10**9)
 input = sys.stdin.readline 
 
 n = int(input())
-graph  = [[] for _ in range(n + 1)]
+graph = [[] for _ in range(n + 1)]
 visited = [False] * (n + 1)
 
 for _ in range(n - 1):
-  u, v = map(int, input().split())
-  graph[u].append(v)
-  graph[v].append(u)
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
 
-# [정점 번호][얼리 어답터 체크]
 dp = [[0, 0] for _ in range(n + 1)]
 
-def solve_dp(num):
-  visited[num] = True
-  dp[num][0] = 0
-  dp[num][1] = 1 # 자신 포함시키기(얼리어답터 수)
+def dfs(node):
+    visited[node] = True
+    dp[node][0] = 0
+    dp[node][1] = 1 
 
-  for i in graph[num]:
-    if not visited[i]:
-      solve_dp(i)
-      dp[num][0] += dp[i][1]
-      dp[num][1] += min(dp[i][0], dp[i][1])
+    for neighbor in graph[node]:
+        if not visited[neighbor]:
+            dfs(neighbor)
+            dp[node][0] += dp[neighbor][1]
+            dp[node][1] += min(dp[neighbor][0], dp[neighbor][1])
 
-solve_dp(1)
+dfs(1)
 print(min(dp[1][0], dp[1][1]))
