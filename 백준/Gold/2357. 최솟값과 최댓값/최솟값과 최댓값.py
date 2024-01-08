f7d81d5653202340d39c95c7dd4820ad20902502
GitdohunKim <134466864+GitdohunKim@users.noreply.github.com>
2024-01-08 19:02:03 +0900

    [Gold I] Title: 최솟값과 최댓값, Time: 3168 ms, Memory: 41228 KB -BaekjoonHub

--- 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.py
+++ 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.py
@@ -1,60 +1,34 @@
 import sys
-def input(): return sys.stdin.readline().rstrip()
+input = sys.stdin.readline
 
-class SegmentTree:
-    def __init__(self,n,array):
-        self.n = n
-        self.array = array
-        self.min_tree = [0] * (4*n)
-        self.max_tree = [0] * (4*n)
-        self.INT_MAX = int(1e15)
-        self.INT_MIN = 0
+def init_tree(start, end, index, tree, func):
+    if start == end:
+        tree[index] = integers[start]
+        return tree[index]
+    mid = (start + end) // 2
+    tree[index] = func(init_tree(start, mid, index * 2, tree, func), init_tree(mid + 1, end, index * 2 + 1, tree, func))
+    return tree[index]
 
-        self.min_init(0,n-1,1)
-        self.max_init(0,n-1,1)
+def query(start, end, index, left, right, tree, func, default):
+    if right < start or left > end:
+        return default
+    if left <= start and end <= right:
+        return tree[index]
+    mid = (start + end) // 2
+    return func(query(start, mid, index * 2, left, right, tree, func, default),
+                query(mid + 1, end, index * 2 + 1, left, right, tree, func, default))
 
-    def min_init(self, left, right, idx):
-        if left == right:
-            self.min_tree[idx] = self.array[left]
-            return self.min_tree[idx]
-        mid = (left+right) // 2
-        self.min_tree[idx] = min(self.min_init(left, mid, 2*idx),
-                                 self.min_init(mid+1, right, 2*idx+1))
-        return self.min_tree[idx]
+N, M = map(int, input().split())
+integers = [int(input()) for _ in range(N)]
 
-    def max_init(self, left, right, idx):
-        if left == right:
-            self.max_tree[idx] = self.array[left]
-            return self.max_tree[idx]
-        mid = (left+right) // 2
-        self.max_tree[idx] = max(self.max_init(left, mid, 2*idx),
-                                 self.max_init(mid+1, right, 2*idx+1))
-        return self.max_tree[idx]
+max_tree = [0] * (4 * N)
+min_tree = [float('inf')] * (4 * N)
 
-    def min_query(self, left, right, idx, node_left, node_right):
-        if node_left > right or node_right < left:
-            return self.INT_MAX
-        if left <= node_left and node_right <= right:
-            return self.min_tree[idx]
-        mid = (node_left+node_right) // 2
-        return min(self.min_query(left, right, 2*idx, node_left, mid),
-                   self.min_query(left, right, 2*idx+1, mid+1, node_right))
+init_tree(0, N - 1, 1, max_tree, max)
+init_tree(0, N - 1, 1, min_tree, min)
 
-    def max_query(self, left, right, idx, node_left, node_right):
-        if node_left > right or node_right < left:
-            return self.INT_MIN
-        if left <= node_left and node_right <= right:
-            return self.max_tree[idx]
-        mid = (node_left+node_right) // 2
-        return max(self.max_query(left, right, 2*idx, node_left, mid),
-                   self.max_query(left, right, 2*idx+1, mid+1, node_right))
-
-    def find_min_max(self, left, right):
-        return self.min_query(left, right, 1, 0, self.n-1), self.max_query(left,right, 1, 0, self.n-1)
-
-n, m = map(int,input().split())
-nums = [int(input()) for _ in range(n)]
-segment_tree = SegmentTree(n, nums)
-for _ in range(m):
+for _ in range(M):
     a, b = map(int, input().split())
-    print(*segment_tree.find_min_max(a-1,b-1))
+    ans_min = query(0, N - 1, 1, a - 1, b - 1, min_tree, min, float('inf'))
+    ans_max = query(0, N - 1, 1, a - 1, b - 1, max_tree, max, 0)
+    print(ans_min, ans_max)
