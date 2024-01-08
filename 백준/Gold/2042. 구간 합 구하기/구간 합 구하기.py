0adda258fda8208095686a85b9d50e16d7a900b4
GitdohunKim <134466864+GitdohunKim@users.noreply.github.com>
2024-01-08 18:45:32 +0900

    [Gold I] Title: 구간 합 구하기, Time: 872 ms, Memory: 118160 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
@@ -1,52 +1,54 @@
-# 세그먼트 트리 생성
-def init(start, end, index):
-    if start == end:
-        segment_tree[index] = arr[start]
-        return segment_tree[index]
-    mid = (start + end) // 2
-    segment_tree[index] = init(start, mid, index * 2) + init(mid + 1, end, index * 2 + 1)
-    return segment_tree[index]
-
-
-# 세그먼트 트리에서 조건에 맞는 구간 합 구하기
-def interval_sum(start, end, index, left, right):
-    if left > end or right < start:
-        return 0
-    if left <= start and right >= end:
-        return segment_tree[index]
-    mid = (start + end) // 2
-    return interval_sum(start, mid, index * 2, left, right) + interval_sum(mid + 1, end, index * 2 + 1, left, right)
-
-
-# 배열의 원소가 변경되었을 때 세그먼트 트리도 변경하기
-def update(start, end, index, what, value):
-    if what < start or what > end:
-        return
-    segment_tree[index] += value
-    if start == end:
-        return
-    mid = (start + end) // 2
-    update(start, mid, index * 2, what, value)
-    update(mid + 1, end, index * 2 + 1, what, value)
-
-
 import sys
 
-input = sys.stdin.readline
-N, M, K = map(int, input().split())
-arr = []
-for _ in range(N):
-    arr.append(int(input()))
+def main():
+    input = sys.stdin.readline
+    n, m, k = map(int, input().split())
+    size = 1 << (len(bin(n)) - 2)
+    size_times_2 = size * 2
+    n_plus_1 = n + 1
+    tree = [0] * size_times_2
 
-segment_tree = [0] * (N * 4)
+    for i in range(size + 1, size + n_plus_1):
+        tree[i] = int(input())
 
-init(0, N - 1, 1)
-for _ in range(M + K):
-    a, b, c = map(int, input().split())
-    if a == 1:
-        b = b - 1
-        diff = c - arr[b]
-        arr[b] = c
-        update(0, N - 1, 1, b, diff)
-    elif a == 2:
-        print(interval_sum(0, N - 1, 1, b - 1, c - 1))
+    for i in range(size - 1, 0, -1):
+        tmp = i * 2
+        tree[i] = tree[tmp] + tree[tmp + 1]
+
+    stack = []
+
+    for _ in range(m + k):
+        a, b, c = map(int, input().split())
+
+        if a == 1:
+            b += size
+            tree[b] = c
+            b >>= 1
+
+            while b:
+                tmp = b * 2
+                tree[b] = tree[tmp] + tree[tmp + 1]
+                b >>= 1
+        else:
+            ans = 0
+            stack.append((1, 0, size, b, c + 1))
+
+            while stack:
+                i, s, e, p, q = stack.pop()
+
+                if s == p and e == q:
+                    ans += tree[i]
+                    continue
+
+                k = (s + e) >> 1
+                i *= 2
+
+                if p < k:
+                    stack.append((i, s, k, p, min(q, k)))
+
+                if q > k:
+                    stack.append((i + 1, k, e, max(p, k), q))
+
+            print(ans)
+
+main()
