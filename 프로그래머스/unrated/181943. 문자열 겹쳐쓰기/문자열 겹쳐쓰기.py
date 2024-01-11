51c22e5a02529830d6021093bba1b0ed70bf1bd0
GitdohunKim <134466864+GitdohunKim@users.noreply.github.com>
2024-01-11 15:36:34 +0900

    [level unrated] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -0,0 +1,6 @@
+def solution(my_string, overwrite_string, s):
+    my_string = my_string[:s] + overwrite_string + my_string[s+len(overwrite_string):]
+    answer = my_string
+    print(answer)
+    return answer
+
